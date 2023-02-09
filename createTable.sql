77e32b5ca7d848a213a49d63e1b93a8cf920a640
eliasjp <illumine1998@gmail.com>
2023-02-09 20:55:28 -0300

    change name VARCHAR(70) to (50)

--- createTable.sql
+++ createTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE movies (
     id SERIAL NOT NULL,
-    name VARCHAR(70) UNIQUE NOT NULL,
+    name VARCHAR(50) UNIQUE NOT NULL,
     description TEXT,
     duration INTEGER NOT NULL,
     price INTEGER NOT NULL
